c5181893a1a9e6df43f0bcf535f2d85365d6e660
msheikomar <msheikomar@paloaltonetworks.com>
2019-12-25 23:21:18 +0530

    Initial Commit

--- Calc.py
+++ Calc.py
@@ -1,14 +1,19 @@
-def add(x,y):
+def add(x, y):
+    print("Hello")
     pass
 
-def subtract(x,y):
+
+def subtract(x, y):
     pass
 
-def multiply(x,y):
-    return x*y
 
-def divide(x,y):
-    return x/y
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
 
 def square(x):
     pass
